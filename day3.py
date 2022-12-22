1d6d3777aa2e95422d9701b987ff53bf7b482b00
elchibek123 <elchibek.kamalov@gmail.com>
2022-12-22 10:34:33 -0600

    added BMI Calculator

--- day3.py
+++ day3.py
@@ -23,3 +23,22 @@ if number % 2 == 0:
     print("This is an even number.")
 else:
     print("This is an odd number.")
+
+# BMI Calculator
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+bmi = round(weight / height ** 2)
+
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi > 18.5 and bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi > 25 and bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi > 30 and bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
