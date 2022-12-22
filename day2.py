97df3f747f337c010dc55b4bede3516cc7f307b8
elchibek123 <elchibek.kamalov@gmail.com>
2022-12-22 09:02:56 -0600

    added day2.py

--- day2.py
+++ day2.py
@@ -1,3 +1,5 @@
+# Tip calculator
+
 print("Welcome to the tip calculator")
 bill = float(input("What was the total bill? "))
 tip = int(input("Waht percentage tip would you like to give? 10, 12, or 15? "))
