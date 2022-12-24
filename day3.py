01dda2a2b2aaaf6c8c1159801236080c159fc197
elchibek123 <elchibek.kamalov@gmail.com>
2022-12-23 18:55:08 -0600

    added love score

--- day3.py
+++ day3.py
@@ -95,3 +95,35 @@ if extra_cheese == "Y":
     bill += 1
 
 print(f"Your final bill is: ${bill}.")
+
+# Love score
+
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+combined_string = name1 + name2
+lower_case_string = combined_string.lower()
+
+t = lower_case_string.count("t")
+r = lower_case_string.count("r")
+u = lower_case_string.count("u")
+e = lower_case_string.count("e")
+
+true = t + r + u + e
+
+l = lower_case_string.count("l")
+o = lower_case_string.count("o")
+v = lower_case_string.count("v")
+e = lower_case_string.count("e")
+
+love = l + o + v + e
+
+score = int(str(true) + str(love))
+
+if score <= 10 or score >= 90:
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif score >= 40 and score <= 50:
+    print(f"Your score is {score}, you are alright tigether.")
+else:
+    print(f"Your score is {score}.")
