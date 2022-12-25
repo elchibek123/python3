57294f00ac55fb1ef3214c82b1b454e6edae1f50
elchibek123 <elchibek.kamalov@gmail.com>
2022-12-25 11:36:33 -0600

    added FizzBuzz

--- day5.py
+++ day5.py
@@ -53,3 +53,15 @@ for number in range(1, 101):
     if number % 2 == 0:
         total2 += number
 print(total)
+
+# FizzBuzz
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
