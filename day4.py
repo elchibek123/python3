8d1e97b0c9217b9f22bb68dda691acbadbb3ef64
elchibek123 <elchibek.kamalov@gmail.com>
2022-12-24 19:49:24 -0600

    added Treasure Map

--- day4.py
+++ day4.py
@@ -33,3 +33,19 @@ names = names_string.split(", ")
 random_person = random.choice(names)
 
 print(f"{random_person} is going to buy the meal today!")
+
+# Treasure Map
+
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+horizontal = int(position[0])
+vertical = int(position[1])
+
+map[vertical - 1][horizontal -1] = "X "
+
+print(f"{row1}\n{row2}\n{row3}")
