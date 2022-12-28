5cf5c79c761cf143f0d13b9ad0d5939574929fd1
elchibek123 <elchibek.kamalov@gmail.com>
2022-12-27 19:52:56 -0600

    updated day7.py

--- day7.py
+++ day7.py
@@ -1,18 +1,51 @@
 # Hangman
 
 import random
-word_list = ["aardvark", "baboon", "camel"]
+
+from hangman_words import word_list
+
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
+end_of_game = False
+lives = 6
+
+from hangman_art import logo
+print(logo)
+
+#Create blanks
 display = []
-for letter in range(len(chosen_word)):
-  display += "_"
+for _ in range(word_length):
+    display += "_"
 
-guess = input("Guess a letter: ").lower()
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-for position in range(len(chosen_word)):
-  letter = chosen_word[position]
-  if letter == guess:
-    display[position] = letter
+    if guess in display:
+        print(f"You've already guessed {guess}")
 
-print(display)
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    from hangman_art import stages
+    print(stages[lives])
