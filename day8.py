a083f97cfcf80bffe98cd29d63ca353dc57a73bb
elchibek123 <elchibek.kamalov@gmail.com>
2022-12-29 19:02:18 -0600

    added day8.py

---
+++ day8.py
@@ -0,0 +1,13 @@
+# Paint Area Calculator
+
+import math
+
+def paint_calc(height, width, cover):
+    area = test_h * test_w
+    num_of_cans = math.ceil(area / coverage)
+    print(f"You'll need {num_of_cans} cans of paint.")
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
