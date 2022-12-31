f8e2f9f5388fda86962a120b63dd2d32dabb5f7a
elchibek123 <elchibek.kamalov@gmail.com>
2022-12-31 17:21:06 -0600

    added Nesting Dictionary

--- day9.py
+++ day9.py
@@ -25,9 +25,45 @@ for key  in programming_dictionary:
   print(key)
   print(programming_dictionary[key])
 
+# Nesting
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin"
+}
+
+# Nesting a List in a Dictionary
+
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+}
+
+# Nesting Dictionary in a Dictionary
+
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5}
+}
+
+# Nesting Dictionary in a List
+
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5
+    }
+]
+
   # Grading Program
 
-  student_scores = {
+student_scores = {
   "Harry": 81,
   "Ron": 78,
   "Hermione": 99, 
