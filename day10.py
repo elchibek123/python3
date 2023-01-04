ea7d31100d22625842cf187b66bdc01826f7db44
elchibek123 <elchibek.kamalov@gmail.com>
2023-01-04 16:17:55 -0600

    added calculator

--- day10.py
+++ day10.py
@@ -37,3 +37,35 @@ year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
 print(days)
+
+# Calculator
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+num1 = int(input("What's the first number?: "))
+
+for symbol in operations:
+  print(symbol)
+operation_symbol = input("Puck an operation from the line above: ")
+num2 = int(input("What's the second number?: "))
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
