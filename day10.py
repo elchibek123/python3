6fe683ce17a4af20e68adf76f79d92179f71464b
elchibek123 <elchibek.kamalov@gmail.com>
2023-01-03 18:51:17 -0600

    added day10.py

---
+++ day10.py
@@ -0,0 +1,11 @@
+# Functions with Outputs
+
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
+
+formatted_string = format_name(input("What is your first name? "), input("What is your last name? "))
+print(formatted_string)
