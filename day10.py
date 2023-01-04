65c237dd21b9083926e38fa9eb40695ab54a0ab5
elchibek123 <elchibek.kamalov@gmail.com>
2023-01-04 17:08:17 -0600

    complete calculator

--- day10.py
+++ day10.py
@@ -40,6 +40,8 @@ print(days)
 
 # Calculator
 
+from calculator_art import logo
+
 def add(n1, n2):
   return n1 + n2
 
@@ -59,13 +61,28 @@ operations = {
   "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
+def calculator():
+  print(logo)
 
-for symbol in operations:
-  print(symbol)
-operation_symbol = input("Puck an operation from the line above: ")
-num2 = int(input("What's the second number?: "))
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+  num1 = float(input("What's the first number?: "))
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+  for symbol in operations:
+    print(symbol)
+
+  should_continue = True
+
+  while should_continue:
+    operation_symbol = input("Puck an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new claculation: ") == "y":
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
+    
+calculator()
